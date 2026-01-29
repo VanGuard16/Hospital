b96e91e6a1cb5416615a8b10ca287ea5c338cc54
VanGuard16 <ibrahimkhidir25@gmail.com>
2026-01-29 18:33:43 +0100

    Fix STATIC_ROOT and MEDIA_ROOT configurations

--- hospital/settings.py
+++ hospital/settings.py
@@ -122,13 +122,14 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = 'login'
